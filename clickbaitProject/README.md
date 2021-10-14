1ddda9939767f987932f5dbe9af447b9aff31f08
Hayden Long <hql5426@psu.edu>
2021-10-14 14:25:52 -0400

    adding another comitt to test

--- clickbaitProject/README.md
+++
@@ -1,2 +0,0 @@
-# clickbaitProject
-Creating an ML model and pipeline to classify tweets/article titles based off of clickbait values
---
+++ data_prepper.py
@@ -0,0 +1,16 @@
+import numpy as np
+import json
+
+dataArray = []
+
+numpy_dataArray = np.array(dataArray)
+
+test_url = ""
+
+train_url = ""
+
+def getData():
+
+
+    
+    return 
